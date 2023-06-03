b61f33461cd81ac62ea64ef424e55a55087a3e59
boat2500 <59160585@go.buu.ac.th>
2023-06-04 01:09:16 +0700

    Can be Send data to fire base server

---
+++ Test/Test_firebase/app/src/main/java/com/example/test_firebase/Users.kt
@@ -0,0 +1,12 @@
+package com.example.test_firebase
+
+import java.net.PasswordAuthentication
+
+data class Users(
+    val username: String?,
+    val firstname: String?,
+    val lastname: String,
+    val password: String
+
+
+)
